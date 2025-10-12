402d837bbe5fbc9366be275b687a467a19b02d8e
oaoivan <ivannaumov007@gmail.com>
2025-10-12 22:22:55 +0300

    +

--- test_scripts/redis/hset_batch_check.go
+++ test_scripts/redis/hset_batch_check.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
 
-    redis "github.com/go-redis/redis/v8"
+	redis "github.com/go-redis/redis/v8"
 )
 
 func main() {
-    ctx := context.Background()
-    client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6380"})
-    defer client.Close()
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6380"})
+	defer client.Close()
 
-    log.Printf("ping redis err=%v", client.Ping(ctx).Err())
+	log.Printf("ping redis err=%v", client.Ping(ctx).Err())
 
-    infoServer := client.Info(ctx, "server")
-    serverInfo, infoErr := infoServer.Result()
-    log.Printf("redis info server err=%v", infoErr)
-    log.Printf("redis info server=%q", serverInfo)
+	infoServer := client.Info(ctx, "server")
+	serverInfo, infoErr := infoServer.Result()
+	log.Printf("redis info server err=%v", infoErr)
+	log.Printf("redis info server=%q", serverInfo)
 
-    infoCmd := client.Do(ctx, "COMMAND", "INFO", "HSET")
-    cmdResult, cmdErr := infoCmd.Result()
-    log.Printf("redis command info err=%v", cmdErr)
-    log.Printf("redis command info=%#v", cmdResult)
+	infoCmd := client.Do(ctx, "COMMAND", "INFO", "HSET")
+	cmdResult, cmdErr := infoCmd.Result()
+	log.Printf("redis command info err=%v", cmdErr)
+	log.Printf("redis command info=%#v", cmdResult)
 
-    payload := []interface{}{"HSET", "test:hsetbatch", "price", "11.5", "timestamp", "1234567890", "exchange", "okx", "symbol", "ILV-USDT"}
-    log.Printf("payload len=%d", len(payload))
+	payload := []interface{}{"HSET", "test:hsetbatch", "price", "11.5", "timestamp", "1234567890", "exchange", "okx", "symbol", "ILV-USDT"}
+	log.Printf("payload len=%d", len(payload))
 
-    err := client.Do(ctx, payload...).Err()
-    log.Printf("hset batch err=%v", err)
+	err := client.Do(ctx, payload...).Err()
+	log.Printf("hset batch err=%v", err)
 }
